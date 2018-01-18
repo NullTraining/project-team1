964576a35ca1020931ece771c4e88ff666f6191c
Tomo Šala <hcs.omot@gmail.com>
2018-01-18 12:58:55 +0100

    Issue #73 - Add data fixtures for Post entity
    We need some dummy/testing data in our databases while developing, so
    I'm adding two very simple dummy posts.

---
+++ src/DataFixtures/ORM/PostsFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures\ORM;
+
+use App\Entity\Post;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class PostsFixtures extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $postOne = new Post(1234, 'Post One Title', 'Post One Content', new \DateTime('now'), 'Post One Author', 'Post One Category');
+
+        $postTwo = new Post(4321, 'Post Two Title', 'Post Two Content', new \DateTime('now'), 'Post Two Author', 'Post Two Category');
+
+        $manager->persist($postOne);
+        $manager->persist($postTwo);
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 10;
+    }
+}
