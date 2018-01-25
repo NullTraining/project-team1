d657a476a5d26133df7efce59ccc5be266e9ad97
flackovic <filackovic@gmail.com>
2018-01-25 21:47:45 +0100

    Fix code style with cs-fixer

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-
-use App\Entity\Post;
 
 class PostController extends Controller
 {
