47189d50374a3302fc806721c37c91d06b6e3709
Tomo Šala <hcs.omot@gmail.com>
2018-02-07 23:42:28 +0100

    Iss #36 [BB] PostCRepository extends ServiceEntityRepository
    PostRepository now extends ServiceEntityRepository so it can create
    Doctrine's query builder.

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-use Doctrine\ORM\EntityRepository;
+use App\Entity\Post;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 
-class PostRepository extends EntityRepository
+class PostRepository extends ServiceEntityRepository
 {
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Post::class);
+    }
 }
