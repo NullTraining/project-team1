5be9b53dba714c7626bd06bee6941ec55399efcc
flackovic <filackovic@gmail.com>
2018-02-08 18:18:50 +0100

    Add routes test

---
+++ tests/Route/HomepageControllerRoutesTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Route;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomepageControllerRoutesTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function urlProvider()
+    {
+        yield ['/'];
+    }
+}
