8b1ac11bc65935e83cfde42348aa100bf1bd8cab
Tomo Šala <hcs.omot@gmail.com>
2018-02-24 23:18:14 +0100

    Iss #125 Configure phpstan - fix PostCommentFixtures
    Add use statements and phpdocs for PostCommentFixtures

--- src/DataFixtures/ORM/PostCommentsFixtures.php
+++ src/DataFixtures/ORM/PostCommentsFixtures.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\DataFixtures\ORM;
 
+use App\Entity\Post;
 use App\Entity\PostComment;
+use App\Entity\User;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -13,20 +15,27 @@ class PostCommentsFixtures extends AbstractFixture implements OrderedFixtureInte
 {
     public function load(ObjectManager $manager)
     {
+        /** @var Post $declarationPost */
         $declarationPost    = $this->getReference('declarationPost');
+
+        /** @var Post $businessReportPost */
         $businessReportPost = $this->getReference('businessReportPost');
 
+        /** @var User $regularUser */
         $regularUser = $this->getReference('user-regular');
+
+        /** @var User $adminUser */
         $adminUser   = $this->getReference('user-admin');
 
+        /** @var PostComment $declarationPostComment */
         $declarationPostComment    = new PostComment();
         $declarationPostComment->setId(1234);
         $declarationPostComment->setComment('I approve of this declaration!');
         $declarationPostComment->setPost($declarationPost);
         $declarationPostComment->setUser($regularUser);
 
+        /** @var PostComment $businessReportPostComment */
         $businessReportPostComment = new PostComment();
-
         $businessReportPostComment->setId(4321);
         $businessReportPostComment->setComment('I\'m taking the biggest cut of profit for myself! HAHA! SO LONG, SUCKERS!!');
         $businessReportPostComment->setPost($businessReportPost);
