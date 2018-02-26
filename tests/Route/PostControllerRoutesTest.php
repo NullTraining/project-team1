c1a37f7272611d3f1f8ab8b7381a49593bd3afd5
flackovic <filackovic@gmail.com>
2018-02-26 13:15:17 +0100

    Update tests to account for login

--- tests/Route/PostControllerRoutesTest.php
+++ tests/Route/PostControllerRoutesTest.php
@@ -2,24 +2,58 @@
 
 namespace App\Tests\Route;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class PostControllerRoutesTest extends WebTestCase
 {
+
+    private $repository;
+    private $client;
+
+    public function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $em = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
+
+        $this->repository = $em->getRepository(User::class);
+
+        $this->user = $this->repository->find(1);
+        $this->client = self::createClient();
+    }
+
     /**
      * @dataProvider urlProvider
      */
     public function testPageIsSuccessful($url)
     {
-        $client = self::createClient();
-        $client->request('GET', $url);
+        $this->logIn();
+        $this->client->request('GET', $url);
 
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
 
     public function urlProvider()
     {
         yield ['/posts'];
         yield ['/posts/show/1'];
+    }
+
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+        $firewallContext = 'main';
+
+        $token = new UsernamePasswordToken($this->user, null, $firewallContext, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewallContext, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
     }
 }
